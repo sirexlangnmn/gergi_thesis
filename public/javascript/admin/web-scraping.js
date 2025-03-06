8553ba0437e4b137345c85b730d37a239004a207
Federex  A  Potolin <potolin.federex@gmail.com>
2025-03-07 02:27:00 +0800

    web scraping js

--- public/javascript/admin/web-scraping.js
+++ public/javascript/admin/web-scraping.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetchBiblioboardApiData('Business Manager');
+    // fetchBiblioboardApiData('Business Manager');
 });
 
 
 async function fetchBiblioboardApiData(searchKeyword) {
     const encoded = encodeURIComponent(searchKeyword);
-    // const url = "https://api.biblioboard.com/search/v2?facet-list=true&limit=20&org-id=1f7368e7-f10b-49a1-8ced-2d9476279974&platform=WEB&offset=0&g=software+engineer";
     const url = `https://api.biblioboard.com/search/v2?facet-list=true&limit=20&org-id=1f7368e7-f10b-49a1-8ced-2d9476279974&platform=WEB&offset=0&g=${encoded}`;
     const publicUrl = `https://openresearchlibrary.org/search-results/g%3D${encoded}`;
+
     console.log('searchKeyword => ', searchKeyword)
     console.log('encoded => ', encoded)
     console.log('url => ', url)
@@ -27,7 +27,7 @@ async function fetchBiblioboardApiData(searchKeyword) {
                 "Sec-Fetch-Site": "cross-site",
                 "Sec-GPC": "1",
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
-                "X-Auth-Token": "665cc804-989e-4f10-a69a-a88828f7fca2",
+                "X-Auth-Token": "6d506236-4324-4eb5-aee3-63f87e25aaa0", // it expires, just get new token to the main website
                 "X-Biblio-Audience": "library.biblioboard.com",
                 "sec-ch-ua": "\"Not(A:Brand\";v=\"99\", \"Brave\";v=\"133\", \"Chromium\";v=\"133\"",
                 "sec-ch-ua-mobile": "?0",
@@ -61,53 +61,20 @@ function generateSearchedData(books, publicUrl) {
         bookElement.innerHTML = `
             <div class="dz-shop-card style-2">
                 <div class="dz-media">
-                    <img src="${book.image}" alt="${book.title}">
+                    <img src="${book.thumbnailUrl}" alt="${book.name}">
                 </div>
                 <div class="dz-content">
                     <div class="dz-header">
                         <div>
-                            <ul class="dz-tags">
-                                dasdsa
-                            </ul>
                             <h4 class="title mb-0"><a href="${publicUrl}">${book.name}</a></h4>
-                        </div>
-                        <div class="price">
-                            <span class="price-num text-primary">dasdsadsadsa</span>
-                            <del>dasdsadsa</del>
                         </div>
                     </div>
                     <div class="dz-body">
-                        <div class="dz-rating-box">
-                            <div>
-                                <p class="dz-para">dasdsadsadsa</p>
-                                <div>
-                                  fdsfdsfsdfdsfdsfsdfdsfsfs
-                                </div>
-                            </div>
-                            <div class="review-num">
-                                <h4>fsdfdsfdsfdsfs</h4>
-                                <ul class="dz-rating">
-                                   fdsfdsfdsfsdfdsfds
-                                </ul>
-                                <span><a href="javascript:void(0);"> Reviews</a></span>
-                            </div>
-                        </div>
-                        <div class="rate">
-                            <ul class="book-info">
-                                <li><span>Writen by</span>dasdasdsa</li>
-                                <li><span>Publisher</span> dasdassa</li>
-                                <li><span>Year</span> dasdasdsa</li>
-                            </ul>
+                        <div class="rate" style="justify-content: none">
                             <div class="d-flex">
                                 <a href="shop-cart.html" class="btn btn-secondary btnhover btnhover2">
-                                    <i class="flaticon-shopping-cart-1 m-r10"></i> Add to cart
+                                    <i class="flaticon-shopping-cart-1 m-r10"></i> Save
                                 </a>
-                                <div class="bookmark-btn style-1">
-                                    <input class="form-check-input" type="checkbox" id="flexCheckDefault">
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                        <i class="flaticon-heart"></i>
-                                    </label>
-                                </div>
                             </div>
                         </div>
                     </div>
@@ -118,3 +85,25 @@ function generateSearchedData(books, publicUrl) {
     });
 }
 
+
+let debounceTimer;
+const inputField = document.getElementById("searchKeyword");
+
+inputField.addEventListener("keyup", (event) => {
+    clearTimeout(debounceTimer);
+    
+    if (event.key === "Enter") {
+        event.preventDefault();
+        getInputValue();
+    } else {
+        debounceTimer = setTimeout(() => {
+            getInputValue();
+        }, 2000);
+    }
+});
+
+function getInputValue() {
+    const inputValue = inputField.value.trim();
+    console.log("Input Value:", inputValue);
+    fetchBiblioboardApiData(inputValue);
+}
